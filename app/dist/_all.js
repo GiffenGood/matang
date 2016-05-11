b0d38c84d01728db1aabe093db9aed00e3eb6be6
mbeckius@gmail.com <MBeckius@gmail.com>
2016-05-11 12:25:01 -0400

    test

---
+++ app/dist/_all.js
@@ -0,0 +1,3 @@
+/// <reference path="../../typings/angularjs/angular.d.ts" />
+/// <reference path="./boot.ts" /> 
+//# sourceMappingURL=_all.js.map
---
+++ app/src/boot.ts
@@ -0,0 +1,5 @@
+/// <reference path="./_all.ts" />
+
+let a = [1,2,3];
+
+let b = [...a,12];
