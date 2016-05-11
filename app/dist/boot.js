6e1bcde778905bb8a69920034abb19b4805d13d0
mbeckius@gmail.com <MBeckius@gmail.com>
2016-05-11 14:31:59 -0400

    theming

--- app/dist/boot.js
+++ app/dist/boot.js
@@ -3,8 +3,11 @@ var ContactManagerApp;
 (function (ContactManagerApp) {
     var app = angular.module('contactManagerApp', ['ngMaterial', 'ngMdIcons']);
     app.controller('mainController', ContactManagerApp.MainController);
-    app.config(function ($mdIconProvider) {
+    app.config(function ($mdIconProvider, $mdThemingProvider) {
         $mdIconProvider.icon('menu', './assets/svg/menu.svg', 24);
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue')
+            .accentPalette('red');
     });
 })(ContactManagerApp || (ContactManagerApp = {}));
 //# sourceMappingURL=boot.js.map
--- app/src/boot.ts
+++ app/src/boot.ts
@@ -5,7 +5,11 @@ module ContactManagerApp{
     var app = angular.module('contactManagerApp',['ngMaterial','ngMdIcons']);
     app.controller('mainController',MainController);
     
-    app.config(($mdIconProvider: angular.material.IIconProvider)=>{
+    app.config(($mdIconProvider: angular.material.IIconProvider,
+                $mdThemingProvider : angular.material.IThemingProvider)=>{
         $mdIconProvider.icon('menu','./assets/svg/menu.svg',24);
+        $mdThemingProvider.theme('default')
+           .primaryPalette('blue')
+           .accentPalette('red'); 
     });
 }
